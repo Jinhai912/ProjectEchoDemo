75b16a1349fee93f7d0eb6a6b5ecf09cd56fb4f3
Jinhai912 <1445874405@qq.com>
2025-07-23 15:47:03 +0800

    修改代码，优化开火逻辑
    优化了开火逻辑

--- Assets/Projects/Scripts/Player/ShootInteraction/Bullet.cs
+++ Assets/Projects/Scripts/Player/ShootInteraction/Bullet.cs
@@ -13,17 +13,25 @@ public class Bullet : MonoBehaviour
     // 当子弹碰到其他物体时，这个函数会被调用
     void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("碰到了");
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+        // 打印碰到的物体的名字，方便调试
+        Debug.Log("碰到了: " + collision.gameObject.name);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // 检查碰到的物体是否是敌人 (假设敌人身上有 Shootable 脚本)
+        if (collision.gameObject.GetComponent<Shootable>() != null)
+        {
+            // 如果是敌人，销毁敌人对象
+            //Destroy(collision.gameObject);
+            
+            // 敌人被销毁后，子弹也应该销毁，而不是继续飞行
+            Destroy(gameObject);
+        }
+        else
+        {
+            // 如果碰到的不是敌人（比如墙壁、地面），也销毁子弹
+            //Destroy(gameObject);
+        }
+
+        // 注意：因为我们设置了物理层，这里的 collision.gameObject 永远不会是玩家自己。
     }
+    
 }
